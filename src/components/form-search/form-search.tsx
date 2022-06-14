78ad2c021aa6e5e41f2bcf84c04e4ac1f3fa0c86
Artur Samoilov <artsamoilov@gmail.com>
2022-06-15 00:23:01 +0300

    fix search list close on blur

--- src/components/form-search/form-search.tsx
+++ src/components/form-search/form-search.tsx
@@ -22,31 +22,31 @@ function FormSearch() {
     };
   }, [searchText]);
 
-  const handleSearchChange = (): void => {
-    setSearchText(String(inputRef.current?.value));
+  const handleSearchListClose = (): void => {
+    setSearchListOpened(false);
+    document.removeEventListener('click', handleSearchListClose);
+  };
+
+  const handleSearchListOpen = (): void => {
     setSearchListOpened(true);
     document.addEventListener('click', handleSearchListClose);
   };
 
-  const handleSearchListClose = (): void => {
-    setSearchListOpened(false);
-    document.removeEventListener('click', handleSearchListClose);
+  const handleSearchChange = (): void => {
+    setSearchListOpened(true);
+    setSearchText(String(inputRef.current?.value));
   };
 
   const getFilteredSearchList = (keyword: string): Guitar[] => guitars.slice().filter(({name}) => name.toLowerCase().includes(keyword.toLowerCase()));
 
   const handleSearchItemClick = (evt: SyntheticEvent): void => {
     searchFormRef.current?.reset();
-    document.removeEventListener('click', handleSearchListClose);
+    handleSearchListClose();
     navigate(`/catalog/item/${evt.currentTarget.id}`);
   };
 
   const handleSearchItemKeypress = (evt: KeyboardEvent): void => {
-    if (evt.key === 'Enter') {
-      searchFormRef.current?.reset();
-      document.removeEventListener('click', handleSearchListClose);
-      navigate(`/catalog/item/${evt.currentTarget.id}`);
-    }
+    evt.key === 'Enter' && handleSearchItemClick(evt);
   };
 
   const handleFormSubmit = (evt: SyntheticEvent): void => {
@@ -54,23 +54,35 @@ function FormSearch() {
     inputRef.current?.focus();
   };
 
+  const handleSearchBlur = (): void => {
+    document.removeEventListener('click', handleSearchListClose);
+    setSearchListOpened(false);
+  };
+
+  const handleFormReset = (): void => {
+    handleSearchListClose();
+    setSearchText('');
+  };
+
   return (
     <div className="form-search">
       <form ref={searchFormRef} onChange={handleSearchChange} onSubmit={handleFormSubmit} className="form-search__form" id="form-search">
-        <button className="form-search__submit" type="submit">
+        <button onBlur={handleSearchBlur} className="form-search__submit" type="submit">
           <svg className="form-search__icon" width="14" height="15" aria-hidden="true">
             <use xlinkHref="#icon-search" />
           </svg>
           <span className="visually-hidden">Начать поиск</span>
         </button>
-        <input ref={inputRef} className="form-search__input" id="search" type="text" autoComplete="off" placeholder="что вы ищите?" />
+        <input onFocus={handleSearchListOpen} ref={inputRef} className="form-search__input" id="search" type="text" autoComplete="off" placeholder="что вы ищите?" />
         <label className="visually-hidden" htmlFor="search">Поиск</label>
       </form>
+
       <ul className={`form-search__select-list ${isSearchListOpened && searchText ? 'list-opened' : 'hidden'}`}>
         {searchList.map(({name, id}) => <li onKeyDown={handleSearchItemKeypress} onClick={handleSearchItemClick} key={id} id={String(id)} className="form-search__select-item" tabIndex={0}>{name}</li>)}
         {searchList.length === 0 && <li style={{paddingTop: '4px', paddingBottom: '4px', fontSize: '14px', lineHeight: '19px'}}>Ничего не найдено</li>}
       </ul>
-      <button onClick={handleSearchListClose} className="form-search__reset" type="reset" form="form-search">
+
+      <button onBlur={handleSearchBlur} onClick={handleFormReset} className="form-search__reset" type="reset" form="form-search">
         <svg className="form-search__icon" width="14" height="15" aria-hidden="true">
           <use xlinkHref="#icon-close" />
         </svg>
