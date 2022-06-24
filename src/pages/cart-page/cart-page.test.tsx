5c3c889ac1f7cacc596cd7a6a14d1cb9e33e93d5
Artur Samoilov <artsamoilov@gmail.com>
2022-06-25 01:42:47 +0300

    update tests

--- src/pages/cart-page/cart-page.test.tsx
+++ src/pages/cart-page/cart-page.test.tsx
@@ -6,8 +6,9 @@ import HistoryRouter from '../../components/history-route/history-route';
 import App from '../../components/app/app';
 import {getMockGuitars, getMockComments} from '../../mocks/mocks';
 import {AppRoute} from '../../const';
+import thunk from 'redux-thunk';
 
-const mockStore = configureMockStore();
+const mockStore = configureMockStore([thunk]);
 
 const fakeGuitars = getMockGuitars();
 const fakeComments = getMockComments(1);
@@ -27,6 +28,8 @@ const store = mockStore({
     comments: fakeComments,
     isCommentsListLoading: false,
     isCommentsListLoaded: true,
+    guitarsSearchList: [fakeGuitars[0]],
+    isSearchListLoaded: true,
   },
 });
 
